035256cde88bfd7db8436b5428e49986a9308d6e
bolunzhang <zhang_bolun@163.com>
2022-07-12 09:21:03 +0800

    second

--- src/main/java/com/bolunzhang/FirstClass.java
+++ src/main/java/com/bolunzhang/FirstClass.java
@@ -3,5 +3,6 @@ package com.bolunzhang;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("orgin");
+        System.out.println("second");
     }
 }
