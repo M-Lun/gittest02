925c8c9a234b3e27bb0de679925149a004631656
bolunzhang <zhang_bolun@163.com>
2022-07-12 09:21:29 +0800

    third

--- src/main/java/com/bolunzhang/FirstClass.java
+++ src/main/java/com/bolunzhang/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("orgin");
         System.out.println("second");
+        System.out.println("third");
     }
 }
